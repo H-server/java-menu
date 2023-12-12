66e4b2f357cd975e9e159e1fa32cf5b904cb7a99
H-server <hani103201@naver.com>
2023-12-12 11:50:35 +0900

    feat: 카테코리 무작위 설정 기능 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,11 @@
 package menu;
 
+import menu.model.Category;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Category category = new Category();
+        String c = category.setCategory();
+        System.out.println(c);
     }
 }
---
+++ src/main/java/menu/model/Category.java
@@ -0,0 +1,45 @@
+package menu.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Category {
+    private static Map<Integer, String> categories = new HashMap<>();
+    private static List<String> selectedCategories = new ArrayList<>();
+
+    public Category() {
+        categories.put(1, "일식");
+        categories.put(2, "한식");
+        categories.put(3, "중식");
+        categories.put(4, "아시안");
+        categories.put(5, "양식");
+    }
+
+    public String setCategory() {
+        String category = null;
+        boolean isDuplicated = true;
+        while(isDuplicated) {
+            category = pickRandomCategory();
+            isDuplicated = validate(category);
+            selectedCategories.add(category);
+        }
+        return category;
+    }
+
+    private static String pickRandomCategory() {
+        return categories.get(Randoms.pickNumberInRange(1, 5));
+    }
+
+    private static boolean validate(String category) {
+        int count = 0;
+        for (String item : selectedCategories) {
+            if(item.equals(category)) {
+                count++;
+            }
+        }
+        return count > 2;
+    }
+}
