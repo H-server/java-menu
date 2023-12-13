c307cdedcbfbffa57990f5e4eb8d2ab680671391
H-server <hani103201@naver.com>
2023-12-13 10:56:14 +0900

    refactor: 사용자 입력 재요청 로직 변경

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,11 +1,10 @@
 package menu;
 
 import menu.controller.MainController;
-import menu.model.Category;
 
 public class Application {
     public static void main(String[] args) {
         MainController mainController = new MainController();
-        mainController.excute();
+        mainController.execute();
     }
 }
--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -1,8 +1,5 @@
 package menu.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import menu.model.Category;
 import menu.model.Coach;
 import menu.model.Recommendation;
@@ -10,42 +7,40 @@ import menu.view.InputView;
 import menu.view.OutputView;
 
 public class MainController {
-    public void excute() {
+    public void execute() {
         OutputView.printStartMessage();
         Coach coach = new Coach();
         String[] coachNames = manageCoachName(coach);
         manageForbiddenMenus(coach, coachNames);
-
         Recommendation recommendation = new Recommendation(coachNames);
         Category category = new Category();
         for(int i=0; i<5; i++) {
             String currentCategory = category.setCategory();
             recommendation.setMenu(currentCategory, coachNames);
-        }
+        } // 월~금 애초에 쫀쫀하게 데이터 저장하는 방법? 0,5 이런 숫자보다.. 한명 한명을 인스턴스로 만들면 편할 듯?!
         OutputView.printMenuResult();
     }
 
     private static String[] manageCoachName(Coach coach) {
-        String[] coachNames = null;
-        boolean isSatisfied = false;
-        while(!isSatisfied) {
+        String[] coachNames;
+        while(true) {
             try {
                 coachNames = coach.setCoachNames(InputView.readCoachNames());
-                isSatisfied = true;
+                break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
         return coachNames;
     }
+    // 코치네임들 데이터 여기로 가져오는 게 맞는가?
 
     private static void manageForbiddenMenus(Coach coach, String[] coachNames) {
         for(String coachName : coachNames) {
-            boolean isSatisfied = false;
-            while(!isSatisfied) {
+            while(true) {
                 try {
-                    coach.setForbiddenMenu(coachName, InputView.readForbiddenMenu(coachName));
-                    isSatisfied = true;
+                    coach.setForbiddenMenu(coachName, InputView.readForbiddenMenu(coachName)); // 여기서는 옳음 view로 전달하기 위함
+                    break;
                 } catch (IllegalArgumentException e) {
                     System.out.println(e.getMessage());
                 }
